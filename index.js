939515136fe78351a1570a922999ba45cf1bd81c
Seu Novo Nome <rickhfbo@gmail.com>
2025-04-15 16:15:56 -0300

    atividade 5

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let numero = parseInt(prompt("Digite um número:"));
-    if (isNaN(numero) || numero < 1) {
-        alert("Digite um número válido (maior ou igual a 1).");
-    
+let num1 = parseInt(prompt("Digite o primeiro número:"));
+    let num2 = parseInt(prompt("Digite o segundo número:"));
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Digite apenas números válidos.");
+        return;
     }
 
+    let inicio = Math.min(num1, num2);
+    let fim = Math.max(num1, num2);
     let resultado = "";
-    for (let i = 1; i <= numero; i++) {
-        resultado += i + " ";
-    } {
-    alert("Números de 1 até " + numero + " : " + resultado);
-}
+
+    for (let i = inicio; i <= fim; i++) {
+        if (i % 2 === 0) {
+            resultado += i + " ";
+        }
+    }
+
+    alert(`Números pares entre ${inicio} e ${fim}:\n${resultado}`);
