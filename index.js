8c470b741d08b397cba2d2592de21d05822619a7
Seu Novo Nome <rickhfbo@gmail.com>
2025-04-15 16:04:44 -0300

    atividade 3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let numero = parseInt(prompt("Digite um número para ver sua tabuada:"));
 
     let resultado = "";
     for (let i = 1; i <= 10; i++) {
-        resultado += `${numero} x ${i} = ${numero * i}\n`;
+        resultado += `${numero} x ${i} = ${numero * i} `;
     } {
     alert("Tabuada de " + numero + " : " + resultado);
 }
